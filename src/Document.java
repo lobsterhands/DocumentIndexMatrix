2fa80a37e0ea38be203d8dcb396aa034dd73b8d1
Lyle Denman <denmanlp@gmail.com>
2016-12-22 14:10:18 -0600

    Store individual Documents message, messageLower, and messageWords

--- src/Document.java
+++ src/Document.java
@@ -5,13 +5,26 @@
 public class Document {
     int id;
     String message;
+    String messageLower;
+    String[] messageWords;
 
     Document(int id, String msg) {
         this.id = id;
         this.message = msg;
+
+        messageLower = message.toLowerCase();
+        messageWords = messageLower.split("\\W+");
     }
 
     String getMessage() {
         return message;
+    }
+
+    String getMessageLower() {
+        return messageLower;
+    }
+
+    String[] getMessageWords() {
+        return messageWords;
     }
 }
