b9e1c4f3543de74ef345a2385f52840983086949
Lyle Denman <denmanlp@gmail.com>
2016-12-22 14:10:58 -0600

    Add List to track all words used.

--- src/DocIndexMatrix.java
+++ src/DocIndexMatrix.java
@@ -1,6 +1,10 @@
 /**
  * Created by Lyle on 12/22/2016.
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DocIndexMatrix {
     public static void main(String[] args) {
 
@@ -10,16 +14,19 @@ public class DocIndexMatrix {
         docs[0] = doc1;
         docs[1] = doc2;
 
-        for (int i = 0; i < docs.length; i++) {
-            String message = docs[i].getMessage();
-            String messageLower = message.toLowerCase();
-            String[] messageWords = messageLower.split("\\W+");
-            System.out.printf("Document %d: %s%n", (i+1), message);
+        int nWords = doc1.getMessageWords().length + doc1.getMessageWords().length;
+        System.out.printf("nWords: %s%n", nWords);
+        List<String> wordList = new ArrayList<>();
 
-            System.out.printf("Document %d words:%n", (i+1));
+        for (int i = 0; i < docs.length; i++) {
+            String[] messageWords = docs[i].getMessageWords();
+            System.out.printf("Document %d: %s%n", (i+1), docs[i].getMessage());
+
             for (int j = 0; j < messageWords.length; j++) {
-                System.out.printf("%s%n", messageWords[j]);
+                wordList.add(messageWords[j]);
             }
         }
+
+        System.out.println("All words used: " + wordList.toString());
     }
 }
