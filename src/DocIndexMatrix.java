ca9a249bf75356bf812b29af9aef516338e3c4d3
Lyle Denman <denmanlp@gmail.com>
2016-12-23 10:23:33 -0600

    Create one additional Document for testing; Altered output format

--- src/DocIndexMatrix.java
+++ src/DocIndexMatrix.java
@@ -9,10 +9,12 @@ public class DocIndexMatrix {
     public static void main(String[] args) {
 
         Document doc1 = new Document(1, "My name is Lyle, and I am cool.");
-        Document doc2 = new Document(1, "My name is Bob, and I am awesome.");
-        Document[] docs = new Document[2];
+        Document doc2 = new Document(2, "My name is Bob, and I am awesome.");
+        Document doc3 = new Document(3, "The brown dog jumps over the lazy fox.");
+        Document[] docs = new Document[3];
         docs[0] = doc1;
         docs[1] = doc2;
+        docs[2] = doc3;
 
         SortedSet<String> wordSet = new TreeSet<>();
 
@@ -23,11 +25,20 @@ public class DocIndexMatrix {
             }
         }
 
+        Object[] str = wordSet.toArray();
+        System.out.printf("%s : ", "All Words");
+        for (int i = 0; i < str.length; i++) {
+            System.out.printf("%-12s", str[i]);
+        }
+        System.out.println();
+
         for(Document d : docs) {
             d.buildMatrix(wordSet);
+            System.out.printf("Document %s: ", d.getId());
             for (int i = 0; i < d.matrix.getColNumber(); i++) {
-                System.out.println(d.getMatrix().getElementAt(0, i));
+                System.out.printf("%-12s", d.getMatrix().getElementAt(0, i));
             }
+            System.out.println();
         }
     }
 }
