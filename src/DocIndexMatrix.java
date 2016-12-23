78c7bf61ac309230d819d411cc7c3f161ee3db99
Lyle Denman <denmanlp@gmail.com>
2016-12-23 15:03:41 -0600

    Created several methods for clarity

--- src/DocIndexMatrix.java
+++ src/DocIndexMatrix.java
@@ -7,38 +7,90 @@ import java.util.TreeSet;
 
 public class DocIndexMatrix {
     public static void main(String[] args) {
-
-        Document doc1 = new Document(1, "My name is Lyle, and I am cool.");
-        Document doc2 = new Document(2, "My name is Bob, and I am awesome.");
-        Document doc3 = new Document(3, "The brown dog jumps over the lazy fox.");
         Document[] docs = new Document[3];
+        Document doc1 = new Document("My name is Lyle, and I am cool.");
         docs[0] = doc1;
+        Document doc2 = new Document("My name is Bob, and I am awesome.");
         docs[1] = doc2;
+        Document doc3 = new Document("The quick brown fox jumps over the lazy dog.");
         docs[2] = doc3;
 
-        SortedSet<String> wordSet = new TreeSet<>();
+        System.out.println("Document Messages:");
+        printDocMessages(docs);
 
-        for (int i = 0; i < docs.length; i++) {
-            String[] messageWords = docs[i].getMessageWords();
-            for(String w : messageWords) {
-                wordSet.add(w);
+        System.out.printf("%nDocument Word Sets:%n");
+        printDocumentWordSets(docs);
+
+        SortedSet<String> wordSet = createSetOfWordsFromDocs(docs);
+        printWordSet(wordSet);
+
+        if (docs.length > 0) {
+            Matrix documentTermMatrix;
+            documentTermMatrix = createDocumentTermMatrix(docs, wordSet);
+            System.out.printf("%nDocument Term Matrix:%n");
+            printMatrix(documentTermMatrix);
+        }
+    }
+
+    private static Matrix createDocumentTermMatrix(Document[] docs, SortedSet allWords) {
+        Matrix matrix = new Matrix(docs.length, allWords.size());
+
+        for (int r = 0; r < docs.length; r++) {
+            SortedSet<?> wordSet = docs[r].getWordSet();
+
+            for (int c = 0; c < allWords.size(); c++) {
+                if (wordSet.contains(allWords.toArray()[c])) {
+                    matrix.setElementAt(r, c, true);
+                } else {
+                    matrix.setElementAt(r, c, false);
+                }
             }
         }
+        return matrix;
+    }
 
-        Object[] str = wordSet.toArray();
+    private static SortedSet createSetOfWordsFromDocs(Document[] docs) {
+        SortedSet<?> wordSet = new TreeSet<>();
+
+        for (Document d : docs) {
+            wordSet.addAll(d.getWordSet());
+        }
+        return wordSet;
+    }
+
+    private static void printWordSet(SortedSet set) {
+        Object[] obj = set.toArray();
         System.out.printf("%s : ", "All Words");
-        for (int i = 0; i < str.length; i++) {
-            System.out.printf("%-12s", str[i]);
+        for (Object o : obj) {
+            System.out.printf("%-9s", o);
         }
         System.out.println();
+    }
 
+    private static void printDocumentWordSets(Document[] docs) {
         for(Document d : docs) {
-            d.buildMatrix(wordSet);
             System.out.printf("Document %s: ", d.getId());
-            for (int i = 0; i < d.matrix.getColNumber(); i++) {
-                System.out.printf("%-12s", d.getMatrix().getElementAt(0, i));
+            System.out.printf("%-9s%n", d.getWordSet().toString());
+        }
+    }
+
+    private static void printMatrix(Matrix m) {
+        int numRows = m.getNumRows();
+        int numCols = m.getNumCols();
+
+        for (int r = 0; r < numRows; r++) {
+            System.out.printf("Document %s: ", r);
+            for (int c = 0; c < numCols; c++) {
+                System.out.printf("%-9s", m.getElementAt(r, c));
             }
             System.out.println();
+        }
+    }
+
+    private static void printDocMessages(Document[] docs) {
+        for(Document d : docs) {
+            System.out.printf("Document %s: ", d.getId());
+            System.out.printf("%-9s%n", d.getMessage());
         }
     }
 }
