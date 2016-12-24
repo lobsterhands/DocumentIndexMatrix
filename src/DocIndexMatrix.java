ef94304b2bca379313a1d92e3b95b08a6789c365
Lyle Denman <denmanlp@gmail.com>
2016-12-23 21:02:30 -0600

    Updated to use a SortedMap to have key/value pairs

--- src/DocIndexMatrix.java
+++ src/DocIndexMatrix.java
@@ -2,8 +2,7 @@
  * Created by Lyle on 12/22/2016.
  */
 
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class DocIndexMatrix {
     public static void main(String[] args) {
@@ -16,70 +15,76 @@ public class DocIndexMatrix {
         docs[2] = doc3;
 
         System.out.println("Document Messages:");
-        printDocMessages(docs);
+        displayDocMessages(docs);
 
         System.out.printf("%nDocument Word Sets:%n");
-        printDocumentWordSets(docs);
+        displayDocumentWordSets(docs);
 
-        SortedSet<String> wordSet = createSetOfWordsFromDocs(docs);
-        printWordSet(wordSet);
+        SortedMap<String, Integer> mapWordSet = createMapOfAllWords(docs);
+        displayMapKeys(mapWordSet);
 
-        if (docs.length > 0) {
-            Matrix documentTermMatrix;
-            documentTermMatrix = createDocumentTermMatrix(docs, wordSet);
-            System.out.printf("%nDocument Term Matrix:%n");
-            printMatrix(documentTermMatrix);
+        Matrix documentTermMatrix;
+        documentTermMatrix = createDocumentTermMatrix(docs, mapWordSet);
+        System.out.printf("%nDocument Term Matrix:%n");
+        displayMatrix(documentTermMatrix);
+
+        String wordToSearch = "am";
+
+        if (mapWordSet.containsKey(wordToSearch)) {
+            System.out.println("Found: '" + wordToSearch + "' at index: " + mapWordSet.get(wordToSearch));
+        } else {
+            System.out.println("'" + wordToSearch + "' not found.");
         }
     }
 
-    private static Matrix createDocumentTermMatrix(Document[] docs, SortedSet allWords) {
-        Matrix matrix = new Matrix(docs.length, allWords.size());
+    private static Matrix createDocumentTermMatrix(Document[] docs, SortedMap<String, Integer> map) {
+        Matrix matrix = new Matrix(docs.length, map.size());
 
         for (int r = 0; r < docs.length; r++) {
-            SortedSet<?> wordSet = docs[r].getWordSet();
+            SortedSet<?> docWordSet = docs[r].getWordSet();
 
-            for (int c = 0; c < allWords.size(); c++) {
-                if (wordSet.contains(allWords.toArray()[c])) {
-                    matrix.setElementAt(r, c, true);
+            for (int c = 0; c < docWordSet.size(); c++) {
+                String str = docWordSet.toArray()[c].toString();
+                if (map.containsKey(str)) {
+                    matrix.setElementAt(r, map.get(str), 1);
                 } else {
-                    matrix.setElementAt(r, c, false);
+                    matrix.setElementAt(r, c, 0);
                 }
             }
         }
         return matrix;
     }
 
-    private static SortedSet createSetOfWordsFromDocs(Document[] docs) {
-        SortedSet<?> wordSet = new TreeSet<>();
+    private static SortedMap createMapOfAllWords(Document[] docs) {
+        SortedMap<String, Integer> mapWordSet = new TreeMap<>();
+        SortedSet<String> sortedSet = new TreeSet<>();
 
         for (Document d : docs) {
-            wordSet.addAll(d.getWordSet());
+            sortedSet.addAll(d.getWordSet());
         }
-        return wordSet;
+
+        int value = 0;
+        for(String key : sortedSet) {
+            mapWordSet.put(key, value);
+            value++;
+        }
+
+        return mapWordSet;
     }
 
-    private static void printWordSet(SortedSet set) {
-        Object[] obj = set.toArray();
-        System.out.printf("%s : ", "All Words");
-        for (Object o : obj) {
-            System.out.printf("%-9s", o);
-        }
-        System.out.println();
-    }
-
-    private static void printDocumentWordSets(Document[] docs) {
+    private static void displayDocumentWordSets(Document[] docs) {
         for(Document d : docs) {
-            System.out.printf("Document %s: ", d.getId());
+            System.out.printf("Document %s: ", d.getId() );
             System.out.printf("%-9s%n", d.getWordSet().toString());
         }
     }
 
-    private static void printMatrix(Matrix m) {
+    private static void displayMatrix(Matrix m) {
         int numRows = m.getNumRows();
         int numCols = m.getNumCols();
 
         for (int r = 0; r < numRows; r++) {
-            System.out.printf("Document %s: ", r);
+            System.out.printf("Document %s: ", (r+1));
             for (int c = 0; c < numCols; c++) {
                 System.out.printf("%-9s", m.getElementAt(r, c));
             }
@@ -87,10 +92,27 @@ public class DocIndexMatrix {
         }
     }
 
-    private static void printDocMessages(Document[] docs) {
+    private static void displayDocMessages(Document[] docs) {
         for(Document d : docs) {
             System.out.printf("Document %s: ", d.getId());
             System.out.printf("%-9s%n", d.getMessage());
+        }
+    }
+
+    private static void displayMapKeys(Map<String, Integer> map) {
+        Set<String> keys = map.keySet();
+        System.out.printf("Map keys:   ");
+        for (String key : keys) {
+            System.out.printf("%-9s", key);
+        }
+    }
+
+    private static void displayMapKeyVal(Map<String, Integer> map) {
+        Set<String> keys = map.keySet();
+        System.out.printf("%nMap contains:%nKey\t\t\t\tValue%n");
+        System.out.printf("---------------------------%n");
+        for (String key : keys) {
+            System.out.printf("%-9s%10s%n", key, map.get(key));
         }
     }
 }
